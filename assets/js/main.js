3bd71d986efac4100472b2e2b2ac6bc99a52beee
Bárbara Martínez Rizo <bmartinezASIR@gmail.com>
2023-08-25 14:54:36 +0200

    Header automatizado con jquery, falta solo el tema por acabar de hacer funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,1 @@
-'use strict'
-
-
-
-// Añadir el atributo lazyload a todas las img
-document.addEventListener("DOMContentLoaded", function() {
-    const imgElements = document.getElementsByTagName("img");
-
-    for (let i = 0; i < imgElements.length; i++) {
-        imgElements[i].setAttribute("loading", "lazy");
-    }
-});
+$(document).ready(function () {});

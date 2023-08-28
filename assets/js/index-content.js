9db7c4834cb14b3cb163e40ac45509376adbe977
Bárbara Martínez Rizo 7S <bmartinezASIR@gmail.com>
2023-08-29 01:55:27 +0200

    api funcional

--- assets/js/index-content.js
+++ assets/js/index-content.js
@@ -7,7 +7,23 @@ $(document).ready(function () {
   }
 
   function posts() {
-    const apiKey = "YpX1p3WBsw2x8qQvv6L192fi";
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://newsnow.p.rapidapi.com/",
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "8b376ea608mshf2d6406f49b4622p1909c7jsn3958de0517e4",
+        "X-RapidAPI-Host": "newsnow.p.rapidapi.com",
+      },
+      processData: false,
+      data: '{\r\n    "text": "Top news",\r\n    "region": "wt-wt"\r\n}',
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
   }
   function generarContenido() {
     banner();

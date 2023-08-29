c6847eb82284a59f4412fef3df361b7c4cad875b
Bárbara Martínez Rizo 7S <bmartinezASIR@gmail.com>
2023-08-29 09:27:01 +0200

    noticias funcionales

--- assets/js/index-content.js
+++ assets/js/index-content.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
   var container = $("#container");
 
+  function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+
   function banner() {
     var banner = $('<section id="banner"></section>');
     container.append(banner);
@@ -22,7 +26,61 @@ $(document).ready(function () {
     };
 
     $.ajax(settings).done(function (response) {
-      console.log(response);
+      var posts = $('<div id="posts"></div>');
+      const newsArray = response.news;
+      console.log(newsArray);
+      newsArray.forEach((newsItem, index) => {
+        var post = $(
+          '<article id="post-' + index + '" class="post"></article>'
+        );
+
+        var img = $(
+          '<div><img src="' +
+            newsItem.image +
+            '" alt="' +
+            newsItem.title +
+            '" title="' +
+            newsItem.title +
+            '"></div>'
+        );
+        var containerTxtPost = $("<div></div>");
+        var title = $("<h2>" + newsItem.title + "</h2>");
+
+        // Procesar la fecha
+        var date = new Date(newsItem.date);
+        var month = date.toLocaleString("default", { month: "long" }); // Obtener el nombre del mes
+        var day = date.getDate(); // Obtener el día del mes
+        var year = date.getFullYear(); // Obtener el año
+
+        var metadata = $(
+          '<div class="meta-data"><span class="source">' +
+            newsItem.source +
+            '</span> | <span class="fecha">' +
+            // capitalizeFirstLetter(month) +
+            month +
+            " " +
+            day +
+            ", " +
+            year +
+            "</span></div>"
+        );
+        var desc = $("<p>" + newsItem.body + "...</p>");
+        var readMore = $(
+          '<a href="' + newsItem.url + '" class="leer-mas">Leer más</a>'
+        );
+
+        containerTxtPost.append(title);
+        containerTxtPost.append(metadata);
+        containerTxtPost.append(desc);
+        containerTxtPost.append(readMore);
+
+        post.append(img);
+        post.append(containerTxtPost);
+
+        posts.append(post);
+
+        container.append(posts);
+      });
     });
   }
   function generarContenido() {
